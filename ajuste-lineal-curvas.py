32589252c28f7de02c24660cad6f7b804350f2fe
sergio1599 <sergioq15@hotmail.com>
2023-08-08 16:21:50 -0500

    fix: changes in method

--- ajuste-lineal-curvas.py
+++ ajuste-lineal-curvas.py
@@ -11,23 +11,24 @@ def ajuste_lineal(x, y):
     x_media = np.mean(x)
     y_media = np.mean(y)
 
-    # Calculamos los coeficientes de la línea recta (m y b)
-    numerador = np.sum((x - x_media) * (y - y_media))
-    denominador = np.sum((x - x_media)**2)
-
+    # numerador = np.sum((x - x_media) * (y - y_media))
+    numerador = len(x) * np.sum(x * y) - np.sum(x) * np.sum(y)
+    # denominador = np.sum((x - x_media)**2)
+    denominador = len(x) * np.sum(x**2) - np.sum(x)**2
     if denominador == 0:
         raise ValueError(
             "El denominador en el cálculo de la pendiente (m) no puede ser cero.")
 
     m = numerador / denominador
     b = y_media - m * x_media
-    print(m, b)
+
+    print(f"La pendiente es: {m}")
+    print(f"El intercepto es: {b}")
 
     return round(m, 5), round(b, 5)
 
 
 try:
-    # Pedimos los datos al usuario
     n_puntos = int(input("Ingrese el número de puntos: "))
     x_data = []
     y_data = []
@@ -37,20 +38,15 @@ try:
         x_data.append(x_val)
         y_data.append(y_val)
 
-    # Realizamos el ajuste lineal
     m, b = ajuste_lineal(np.array(x_data), np.array(y_data))
 
-    # Imprimimos la ecuación de la línea recta
     x, y = sp.symbols('x y')
     ecuacion = sp.Eq(y, m * x + b)
     print(f"\nEcuación de la línea recta: {ecuacion}")
 
-    # Graficamos los puntos
     plt.scatter(x_data, y_data, color="green", label="Datos")
     x_values = np.linspace(min(x_data), max(x_data), 100)
-    # Usamos la ecuación de la recta ajustada 'y = mx + b'
     y_values = m * x_values + b
-    # Graficamos la línea ajustada
     plt.plot(x_values, y_values, color="orange",
              label="Ajuste lineal para: " + str(ecuacion))
     plt.xlabel("X")
